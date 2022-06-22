e83bd7b2314cc7a037dfb79ca16339f0318128db
Timur Zaripov <timur.zaripov.05@gmail.com>
2022-06-22 17:25:28 +0300

    First commit, added Main, StreamMain

---
+++ Task1/src/StreamMain.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.stream.Stream;
+
+public class StreamMain {
+
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        intList.stream()
+                .filter(x -> x > 0)
+                .filter(x -> x % 2 == 0)
+                .sorted(Integer::compareTo)
+                .forEach(System.out::println);
+    }
+}
